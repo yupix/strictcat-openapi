52e30bca9627d8cda81b359583e8b664650617e1
yupix <yupi0982@outlook.jp>
2022-12-16 06:32:24 +0000

    feat: 再帰的なitemsへの対応
    feat: nullableをサポート
    feat: arrayなど明らかにおかしいものをanyに置き換えるように

--- main.py
+++ main.py
@@ -5,18 +5,72 @@ from type import IOpenAPI
 final_content = ''
 schemas = {}
 
+def parse_ref(ref: str):
+    return ref.split('/')[-1]
+
+
+
 with open('./openapi.json', mode='r', encoding='utf-8') as f:
     openapi: IOpenAPI = json.load(f)
+
+    class Property:
+        def __init__(self, property: dict[str, str]) -> None:
+            self.property = property
+            self.property_type = property.keys()
+        
+        def check_nullable(self, nullable: bool):
+            return '?' if nullable else ''
+
+        def parse(self):
+            def check_ref(property: dict[str, str], items:bool=False):
+                property_keys = property.keys()
+                if '$ref' in property_keys:
+                    property_type = parse_ref(property['$ref'])
+                    return property_type + '[]' if items else property_type, self.check_nullable(property.get('nullable',False))
+                elif items and property.get('items'):
+                    if property['items'].get('items'):
+                        if 'items' not in property['items']['items']:
+                            return property['items']['items'], self.check_nullable(property.get('nullable',False))
+                        return check_ref(property['items']['items'], items=True), self.check_nullable(property.get('nullable',False))
+                    else:
+                        return property['items']['type'] + '[]', self.check_nullable(property.get('nullable',False))
+
+
+                elif 'properties' in property_keys:
+                    property_type = {}
+                    for _property in property['properties']:
+                        _property_type = check_ref(property['properties'][_property])
+                        property_type[_property + _property_type[1]] = _property_type[0] if _property_type[0] not in ['array', 'either'] else 'any'
+                    return property_type, self.check_nullable(property.get('nullable',False))
+                elif 'items' in property_keys:
+                    return check_ref(property['items'], items=True)
+                elif 'enum' in property_keys:
+                    property_type = ''
+                    for enum in property['enum']:
+                        property_type += f'\'{enum}\' |'
+                    return property_type.rstrip('|'), self.check_nullable(property.get('nullable',False))
+                else:
+                    return property['type'] if property.get('type') else 'any', self.check_nullable(property.get('nullable',False))
+
+            property_type, nullable = check_ref(self.property)
+
+            print(property_type)
+            if property_type in ['either', 'array']:
+                return 'any', nullable
+
+            return property_type, nullable
+
+        def generate(self):
+            pass
+
     for schema in openapi['components']['schemas']:
-        for propertiy in openapi['components']['schemas'][schema]['properties']:
+        for propertiy_name in openapi['components']['schemas'][schema]['properties']:
+            property = openapi['components']['schemas'][schema]['properties'][propertiy_name]
+            property_type, nullable = Property(property).parse()
             if schemas.get(schema) is None:
-                schemas[schema] = {
-                    propertiy: openapi['components']['schemas'][schema]['properties'][propertiy]['type']
-                }
+                schemas[schema] = {propertiy_name: property_type}
             else:
-                schemas[schema].update({
-                    propertiy: openapi['components']['schemas'][schema]['properties'][propertiy]['type']
-                })
+                schemas[schema].update({propertiy_name: property_type})
         else:
             if schemas.get(schema) is None:
                 schemas[schema] = "any"
@@ -42,16 +96,23 @@ interface {schema} {json.dumps(schemas[schema], ensure_ascii=False).replace('"',
                 content += 'params: ' + json.dumps({i['name']: i['schema']['type'] for i in parameters}, ensure_ascii=False).replace('"', '') + '\n'
             request_body = api.get('requestBody')
             if request_body:
-                content += f'body:' + request_body['content']['application/json']['schema'].get('$ref').split('/')[-1] + '\n'
+                request_type = Property(request_body['content']['application/json']['schema']).parse()
+                if len(request_type[0].keys()) > 0:
+                    content += f'body: ' + json.dumps(request_type[0], ensure_ascii=False).replace('"', '') + '\n'
+            
             responses = api.get('responses')
             if responses:
+                
                 for response_code in responses:
-                    if response_code == '201':
-                        if responses['201'].get('content'):
-                            content += 'response: ' + responses['201']['content']['application/json']['schema'].get('$ref').split('/')[-1]
+                    if response_code == '201' or '200':
+                        if responses[response_code].get('content'):
+                            response_type, nullable = Property(responses[response_code]['content']['application/json']['schema']).parse()
+                            content += 'response: ' + json.dumps(response_type, ensure_ascii=False).replace('"', '')
+
                         else:
                             content += 'response: any'
                     else: content += 'response: any'
+                    break
             else:
                 content += 'response: any'
 
